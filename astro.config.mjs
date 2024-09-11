2d4dfd4282baf4764a59850fe14a0b5f3c95fb02
z1 <aaronzyg@163.com>
2024-09-11 12:03:30 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lost0427.github.io',
-    base: '/Reblog/',
+    base: '/',
     integrations: [mdx(), sitemap(), tailwind()],
 });
